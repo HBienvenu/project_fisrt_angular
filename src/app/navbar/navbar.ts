8edf710c97a334d1bdbe05ecc9fd0dfd6919bfc6
Bienvenu02 <bienvenuhounye05@gmail.com>
2026-03-16 17:19:00 +0100

    finition de la page accueil

--- src/app/navbar/navbar.ts
+++ src/app/navbar/navbar.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
-import { NgOptimizedImage } from '@angular/common';
+import { Component, signal, effect, inject } from '@angular/core';
+import { NgOptimizedImage, isPlatformBrowser } from '@angular/common';
+import { Button } from 'primeng/button';
+import { PLATFORM_ID } from '@angular/core';
+
+const DARK_KEY = 'theme-dark';
 
 @Component({
   selector: 'app-navbar',
-  imports: [NgOptimizedImage],
+  standalone: true,
+  imports: [NgOptimizedImage, Button],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
-export class Navbar {}
+export class Navbar {
+  private platformId = inject(PLATFORM_ID);
+  private isBrowser = isPlatformBrowser(this.platformId);
+
+  isDark = signal<boolean>(false);
+
+  constructor() {
+    if (this.isBrowser) {
+      this.isDark.set(localStorage.getItem(DARK_KEY) === 'true');
+
+      effect(() => {
+        const dark = this.isDark();
+        document.documentElement.classList.toggle('app-dark', dark);
+        localStorage.setItem(DARK_KEY, String(dark));
+      });
+    }
+  }
+
+  toggleDarkMode() {
+    this.isDark.update((v) => !v);
+  }
+}
