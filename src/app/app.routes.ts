79bd7f970075bd548adaae7a8659844995d7baea
Bienvenu02 <bienvenuhounye05@gmail.com>
2026-03-17 13:54:31 +0100

    add

--- src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { PromptList } from './prompts/prompt-list/prompt-list';
-import { PromptForm } from './prompts/prompt-form/prompt-form';
-
-export const routes: Routes = [
-  { path: '', redirectTo: 'prompts', pathMatch: 'full' },
-  { path: 'prompts', component: PromptList },
-  { path: 'prompts/create', component: PromptForm },
-];
